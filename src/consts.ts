a457f748a51105ac69f24c85265e15b6473a303a
Eric Tossell <erictossell@gmail.com>
2023-12-27 02:22:59 -0500

    Trying out Astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Eric\'s Astro Blog';
+export const SITE_TITLE = 'Eriim\'s Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my documentation and technology repo!';
